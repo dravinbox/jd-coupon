5092edb027112ec0c9f06750408463218801d721
ZhangRongYan <kawai566@yeah.net>
2018-11-09 15:22:11 +0800

    init

---
+++ src/main/java/com/hot/jdcoupon/JdCouponApplication.java
@@ -0,0 +1,12 @@
+package com.hot.jdcoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdCouponApplication.class, args);
+    }
+}
